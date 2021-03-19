590a801548c5899227717bd96f585820e7738b9b
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-19 10:15:29 -0500

    commit tree 98a2dc7b6ca93f6afabe9cd9f7b481f3064ed41a

--- main.js
+++ main.js
@@ -4,7 +4,38 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const errorModal = document.getElementById("modal");
+errorModal.classList.add("hidden");
 
+document.addEventListener("DOMContentLoaded", () => {
+  listenForHeartClicks();
+});
+
+function listenForHeartClicks() {
+  const hearts = document.querySelectorAll("span.like-glyph");
+  hearts.forEach(heart => {
+    heart.addEventListener("click", (e) => {
+
+      mimicServerCall()
+        .then(function() {
+          if(e.target.classList.contains("activated-heart")) {
+            e.target.classList.remove("activated-heart");
+            e.target.innerText = EMPTY_HEART;
+          } else {
+            e.target.classList.add("activated-heart");
+            e.target.innerText = FULL_HEART;
+          }
+        })
+        .catch(function(error) {
+          console.log(error.message);
+          errorModal.classList.remove("hidden");
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 5000);
+      })
+    })
+  });
+}
 
 
 //------------------------------------------------------------------------------
